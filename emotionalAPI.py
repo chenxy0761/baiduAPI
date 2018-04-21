b2ea8c4f9de875f0d4eb3367a1d1104112732488
chenyang <cxy404@hotmail.com>
2018-04-21 16:57:50 +0800

    baiduAPI---进行情感分析

--- emotionalAPI.py
+++ emotionalAPI.py
@@ -1,47 +1,49 @@
 # -*- coding:utf-8 -*-
-
+import pymysql
 import requests
 import json
 import jsonpath
 
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+conn = pymysql.connect(host='localhost', port=3306, user='root',
+                       passwd='root', db='test', charset='utf8mb4')
+
 url = 'https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?access_token=24.0a5cfd84db329d3ff26af08885eac866.2592000.1526720122.282335-11126907'
-body = {
-    "text": "充分发挥不要脸的精神。"
-}
 headers = {'content-type': "application/json", }
+i = 1
+while i < 1460:
+    cur = conn.cursor()
+    text = cur.execute('select text from baojing where seq = %s', [i])
+    print(i)
+    if text != "":
+        body = {
+            "text": str(text)[0:1000]
+        }
+        response = requests.post(url, data=json.dumps(body), headers=headers)
+        jsonobj = json.loads(response.text)
 
-# print type(body)
-# print type(json.dumps(body))
-# 这里有个细节，如果body需要json形式的话，需要做处理
-# 可以是data = json.dumps(body)
-response = requests.post(url, data=json.dumps(body), headers=headers)
-# 也可以直接将data字段换成json字段，2.4.3版本之后支持
-# response  = requests.post(url, json = body, headers = headers)
-
-# 返回信息
-print response.text
-# 把json格式字符串转换成python对象
-jsonobj = json.loads(response.text)
-
-# 从根节点开始，匹配name节点
-citylist = jsonpath.jsonpath(jsonobj,'$..sentiment')
-text = jsonpath.jsonpath(jsonobj,'$..text')
-confidence = jsonpath.jsonpath(jsonobj,'$..confidence')
-# citylist = jsonpath.jsonpath(jsonobj,'$..sentiment')
-# citylist = jsonpath.jsonpath(jsonobj,'$..sentiment')
-#
-print(text)
-print(confidence)
-print citylist
-# print type(citylist)
-# fp = open('city.json','w')
-#
-# content = json.dumps(citylist, ensure_ascii=False)
-# print content
-#
-# fp.write(content.encode('utf-8'))
-# fp.close()
+        # 从根节点开始，匹配name节点
+        sentiment = jsonpath.jsonpath(jsonobj, '$..sentiment')[0]  # 0为消极 1为中性 2为积极
+        # text = jsonpath.jsonpath(jsonobj,'$..text')
+        confidence = jsonpath.jsonpath(jsonobj, '$..confidence')[0]
+        items = jsonpath.jsonpath(jsonobj, '$..items')[0]
+        # citylist = jsonpath.jsonpath(jsonobj,'$..sentiment')
+        #
+        # print(confidence)
+        # print sentiment
+        # print(str(items[0]))
+        ops = [str(items[0]), sentiment, i]
+        try:
+            cur.execute("update baojing set json= %s,flag= %s where seq = %s", ops)
+            conn.commit()
+            cur.close()
+        except Exception as e:
+            print(e)
+        i += 1
+    else:
+        i += 1
+conn.close()
